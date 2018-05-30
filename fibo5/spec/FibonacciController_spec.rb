082999825965560bc4056f9b35afee85221660a2
Matuchinn <matias.untref@gmail.com>
2018-05-30 02:50:48 -0300

    Agregado test para validar que el parametro sentido directo funciona correctamente

--- fibo5/spec/FibonacciController_spec.rb
+++ fibo5/spec/FibonacciController_spec.rb
@@ -31,6 +31,17 @@ describe 'Aplicacion Sinatra' do
      expect(cuerpo_parseado['fibonacci']['lista']).to eq [3, 2, 1, 1, 0]
    end
   end
+
+  describe '/fibonacci/5?sentido=directo' do
+   it 'Deberia devolver un json con los primeros 5 numeros de la sucecion de Fibonacci en orden directo' do
+     get '/fibonacci/5?sentido=directo'
+
+     expect(last_response).to be_ok
+     cuerpo_parseado = JSON.parse(last_response.body)
+     expect(cuerpo_parseado['fibonacci']['limite']).to eq 5
+     expect(cuerpo_parseado['fibonacci']['lista']).to eq [0,1,1,2,3]
+   end
+  end
   
   describe '/fibonacci/5/sumatoria' do
    it 'Deberia devolver un json con la suma de los primeros 5 numeros de la sucecion de Fibonacci' do
